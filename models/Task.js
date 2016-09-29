2360419fe65d06e94da0211d0512c870b54b17e7
jinanshshah j <jinanshshah j>
2016-09-29 13:34:54 +0530

    done

--- models/Task.js
+++ models/Task.js
@@ -13,16 +13,15 @@ getTaskById:function(id,callback){
 },
 addTask:function(Task,callback){
     console.log(Task);
-console.log(Task[0].Id);
-console.log("inside");
-return db.query("Insert into task values(?,?,?)",[Task[0].Id,Task[0].Title,Task[0].Status],callback);
+
+return db.query("Insert into task values(?,?,?)",[Task.Id,Task.Title,Task.Status],callback);
 //return db.query("insert into task(Id,Title,Status) values(?,?,?)",[Task1.Id,Task1.Title,Task1.Status],callback);
 },
 deleteTask:function(id,callback){
     return db.query("delete from task where Id=?",[id],callback);
 },
 updateTask:function(id,Task,callback){
-    return  db.query("update task set Title=?,Status=? where Id=?",[Task[0].Title,Task[0].Status,id],callback);
+    return  db.query("update task set Title=?,Status=? where Id=?",[Task.Title,Task.Status,id],callback);
 }
 
 };
