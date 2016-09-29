f15d50a6ad132c0354cec533180a80c52cd5bb11
jinanshshah j <jinanshshah j>
2016-09-29 14:34:24 +0530

    done 1

--- models/Task.js
+++ models/Task.js
@@ -12,8 +12,8 @@ getTaskById:function(id,callback){
     return db.query("select * from task where Id=?",[id],callback);
 },
 addTask:function(Task,callback){
-    console.log(Task);
-
+   console.log("inside service");
+   console.log(Task.Id);
 return db.query("Insert into task values(?,?,?)",[Task.Id,Task.Title,Task.Status],callback);
 //return db.query("insert into task(Id,Title,Status) values(?,?,?)",[Task1.Id,Task1.Title,Task1.Status],callback);
 },
