2067c504c70f2ac5b3cf7399fdd6e7350490893e
jinanshshah j <jinanshshah j>
2016-11-05 20:56:10 +0530

    done

--- dbconnection.js
+++ dbconnection.js
@@ -1,11 +1,15 @@
 var mysql=require('mysql');
 var connection=mysql.createPool({
 
-host:'localhost',
+/*host:'localhost',
 user:'root',
 password:'',
 database:'demo'
-
+*/
+host:'faizal123.db.9462939.hostedresource.com',
+    user:'faizal123',
+    password:'Jinal@9898',
+    database:'faizal123'
 
 });
 module.exports=connection;
